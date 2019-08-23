ac53852b146cbb52e217fca64cb32f45a9de9b90
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-23 04:05:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,13 +45,12 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
-  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
-
+  consolCart = consolidate_cart(cart)
+  couponCart = apply_coupons(consolCart, coupons)
+  discountCart = apply_clearance(couponCart)
   total = 0.0
-  cart_with_discounts_applied.keys.each do |item|
-    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  discountCart.keys.each do |item|
+    total += discountCart[item][:price]*discountCart[item][:count]
   end
-  total > 100.00 ? (total * 0.90).round : total
+  total > 100.00 ? (total * 0.90).round(2) : total
 end
